9efad958adb7f719aa8e2a8c116d8d961dc3fce8
Yauheni Kulik <evgeniy1991@gmail.com>
2018-08-27 20:54:17 +0300

    cleanup

--- js/smart_table.js
+++ js/smart_table.js
@@ -48,15 +48,6 @@ SmartTable.prototype.addElement = function (objForCut){
         td.innerHTML = rez;
     }
 };
-/*function addElement(objForCut) {
-    this.addElement(objForCut);
-
-
-}*/
-/*addElement.prototype=
-    Object.create(SmartTable.prototype);
-addElement.prototype.constructor=addElement;
-*/
 
 SmartTable.prototype.addSomeElements = function (arrSomeEl){
     this.arrSomeEl = arrSomeEl;
@@ -76,33 +67,11 @@ SmartTable.prototype.addSomeElements = function (arrSomeEl){
     }
 };
 
-/*function addSomeElements() {
-    this.addSomeElements(arrSomeEl)
-
-}
-addSomeElements.prototype=
-    Object.create(SmartTable.prototype);
-addSomeElements.prototype.constructor = addSomeElements;*/
-
-//
-
 SmartTable.prototype.deleteAll = function (){
 
     var table = document.getElementById(this.containerId).children[0];
     table.parentNode.removeChild(table);
 };
-
-/*
-function deleteAll() {
-    this.deleteAll();
-}
-
-
-deleteAll.prototype=
-    Object.create(SmartTable.prototype);
-deleteAll.prototype.constructor=deleteAll;
-*/
-
 
 SmartTable.prototype.deleteEl = function (ind){
     var table = document.getElementById(this.containerId).children[0];
@@ -114,87 +83,3 @@ SmartTable.prototype.deleteEl = function (ind){
     }
 
 };
-
-/*function deleteEl(ind) {
-    this.deleteEl(ind);
-}
-deleteEl.prototype=
-    Object.create(SmartTable.prototype);
-deleteEl.prototype.constructor=deleteEl;*/
-//smartTable.deleteEl(idx)
-
-
-
-/* table.deleteElement()
-table.deleteAll()
-deleteElement пока не знаю как нам реализовать
-опустим*/
-/*    В конструктуре избаваиться от аргументов, на этот раз будет всего один аргумент options
-    options - это будет объект js
-    пример
-    var table = new SmartTable({
-    data: [ [dsds, dsds,dsds]...],
-    containerId: 'id',
-    ....
-    })
-второе задание: избавиться от двумерного массива
-    у тебя на этот раз будет массив обьектов
-    т.к. это распрастраненная практика в таблицах
-    всегда почти работаем с массивом объектов
-    к примеру у тебя данные должны быть вида
-	типа так должно работать
-    data и columns обязательные параметры для таблицы
-	Ты строишь свою таблицу на основе колонок
-	Т.е. В columns хранится информация как извлекать данные и в какую колонку данные нужно записывать
-*/
-/*переделать функции addElement
-добавить в прототипа SmartTable функцию addElement., которая будет принимать один аргумент - объект
-{
-id: 1,
-firstName: 'Vasya',
-lastName: 'Ivanov',
-age: 12,
-birthDate: '16.07.1991'
-}
-var s = {
-id: 1,
-firstName: 'Vasya',
-lastName: 'Ivanov',
-age: 12,
-birthDate: '16.07.1991'
-}
-
-
-var table = new SmartTable(......
-table.addElement(s)
-и соотвественно addElements
-var data = [
-{
-id: 1,
-firstName: 'Vasya',
-lastName: 'Ivanov',
-age: 12,
-birthDate: '16.07.1991'
-},
-{
-id: 2,
-firstName: 'Vasya',
-lastName: 'Ivanov',
-age: 12,
-birthDate: '16.07.1991'
-},
-{
-id: 3,
-firstName: 'Vasya',
-lastName: 'Ivanov',
-age: 12,
-birthDate: '16.07.1991'
-}
-]
-table.addElements(data)*/
-/*написать функцию удаления
-smartTable.delete(idx)
-idx - номер эелемента или строки которую нужно удалить
-первый элемент принять за 0, т.е. отсчет идет с 0
-удалять эелемент из data и html*/
-
