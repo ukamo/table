a2bd6505d8499bb099223de9fdeec111d0981f85
Julia Morozova <ukamo125@gmail.com>
2018-08-28 21:46:00 +0300

    add function styleBackground? change style 28.08.2018

--- js/smart_table.js
+++ js/smart_table.js
@@ -20,11 +20,19 @@ SmartTable.prototype.drawArray = function (){
     for ( i =0; i <this.arr.length;i++){//по колонкам а внуитри-по массиву
         var tr = document.createElement('tr');
         table.appendChild(tr);
+        var elementObj = this.arr[i];
+        for(var key in elementObj){
+            if(key == 'id'){
+                var nameDataId = elementObj[key];
+                tr.setAttribute('data-id',nameDataId)
+
+            }
+        }
         for (var j=0; j <this.colums.length;j++){
-            var elementObj = this.arr[i];
+
             var column = this.colums[j];
             //for(var key in elementObj){
-            var rezForCutTable = elementObj[column.propertyName]
+            var rezForCutTable = elementObj[column.propertyName];
             var td = document.createElement('td');
             tr.appendChild(td);
             if (rezForCutTable instanceof Date){
@@ -40,9 +48,16 @@ SmartTable.prototype.addElement = function (objForCut){
     var table = document.getElementById(this.containerId).children[0];
     var tr = document.createElement('tr');
     table.appendChild(tr);
+    for(var key in objForCut){
+        if(key == 'id'){
+            var nameDataId = objForCut[key]
+            tr.setAttribute('data-id',nameDataId)
+
+        }
+    }
     for (var j=0; j <this.colums.length;j++){
         var column = this.colums[j];
-        var rez =objForCut[column.propertyName]
+        var rez =objForCut[column.propertyName];
         var td = document.createElement('td');
         tr.appendChild(td);
         td.innerHTML = rez;
@@ -51,7 +66,7 @@ SmartTable.prototype.addElement = function (objForCut){
 
 SmartTable.prototype.addSomeElements = function (arrSomeEl){
     this.arrSomeEl = arrSomeEl;
-    var table = document.getElementById(this.containerId).children[0];
+    //  var table = document.getElementById(this.containerId).children[0];
     for ( var i=0; i <this.arrSomeEl.length;i++){
         this.addElement(this.arrSomeEl[i]);
         /*var tr = document.createElement('tr');
@@ -78,9 +93,38 @@ SmartTable.prototype.deleteEl = function (ind){
     var table = document.getElementById(this.containerId).children[0];
     var trs = table.rows;
     for(var i=0; i<trs.length; i++){
-        if(i == ind){
+        var indexForData = trs[i].getAttribute('data-id');
+        if(indexForData == ind){
             table.deleteRow(i)
         }
     }
+    /*  var trs = table.rows;
+      for(var i=0; i<trs.length; i++){
+          if(i == ind){
+              table.deleteRow(i)
+          }
+      }*/
+
 
 };
+
+/*Связать данные в таблице с htm
+Для этого добавить в каждую tr таблицы атрибут data-id
+Типа <tr data-id=1...>...
+Это нужно чтобы к соответствующим объектам была соответствующа строчка в таблице*/
+/*
+теперь нужно добавить выделение строчки в таблице
+если я нажму на строчку она должна выдлеиться
+если на другую то должна выделиться другая*/
+
+/*
+теперь сделай так чтобы выделенная строчка по кнопке удалить строчку - удалялась
+корректно
+и из data
+и из html
+т.е. в smart Table у тебя есть свойство arr переименуй это в data
+data должна быть всегда синхронизирована с html
+*/
+
+
+
